a7debc168a49bcbefb4768962032095e65a69b67
Marc Miró <mmirosanchez@gmail.com>
2023-12-24 01:53:18 +0100

    Rework again all files and finish brew

--- init.sh
+++ init.sh
@@ -1,20 +1,31 @@
 #!/bin/bash
 # Script to initialize a new workstation
 
-# Check if Homebrew is installed, install if we don't have it
+# Check if Homebrew is installed, install if we don't have it or upgrade it if we do
 if test ! $(which brew); then
   echo "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+  echo "Updating homebrew..."
+  brew update
 fi
-
-# Update Homebrew recipes
-brew update
 
 # Install Ansible
 if test ! $(which ansible); then
   echo "Installing Ansible..."
   brew install ansible
+else
+  echo "Upgrading Ansible..."
+  brew upgrade ansible
 fi
 
 # Launch Ansible playbook
-ansible-playbook -i hosts main.yml --ask-become-pass
+echo "Launching playbook with check parameter..."
+ansible-playbook -i hosts main.yml --ask-become-pass --check
+
+# Launch Ansible playbook
+read -r -p "Do you want to proceed without check parameter? [y/N]" -n 1
+if [[ "$REPLY" =~ ^[Yy]$ ]]; then
+  echo -e "\nLaunching playbook without check parameter..."
+  ansible-playbook -i hosts main.yml --ask-become-pass
+fi
