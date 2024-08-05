f541522b5df398af3f5917d3c809212f803101d6
Marc Miró <44055899+marcmiro@users.noreply.github.com>
2024-08-05 13:34:13 +0200

    Fix brew installation and add google-chrome to brew list

--- init.sh
+++ init.sh
@@ -5,6 +5,8 @@
 if test ! $(which brew); then
   echo "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/marc.miro/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 else
   echo "Updating homebrew..."
   brew update
